dd01d91f31a2b505f9c7c6899543bd19511d0ebe
Sirrat21 <sirratrashid@gmail.com>
2024-12-21 00:55:14 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const countdown = () => {
+    const now = new Date();
+    const newYear = new Date(`January 1, ${now.getFullYear() + 1} 00:00:00`);
+    const diff = newYear - now;
+  
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((diff / (1000 * 60)) % 60);
+    const seconds = Math.floor((diff / 1000) % 60);
+  
+    document.getElementById("days").textContent = days.toString().padStart(2, "0");
+    document.getElementById("hours").textContent = hours.toString().padStart(2, "0");
+    document.getElementById("minutes").textContent = minutes.toString().padStart(2, "0");
+    document.getElementById("seconds").textContent = seconds.toString().padStart(2, "0");
+  };
+  
+  setInterval(countdown, 1000);
+  countdown();
+  
